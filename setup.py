56ac8251657fbc76280ebdc7d254e8f76e0edb6e
Neal Jackson <nealsjackson@gmail.com>
2025-04-14 11:32:48 -0600

    remove

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bil_name',
-      version='0.0.1',
+      version='0.0.2',
       description='Converts a 64-bit hex string to a human-readable "adj_name" name',
       url='https://github.com/blueirislabs/bil_name',
       author='Neal Jackson',
